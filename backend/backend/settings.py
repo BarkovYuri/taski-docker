96caa5b5dbc28ea664a5f8443a2d251ddb5df53d
Barkov Yuri <fertikylyapka@yandex.ru>
2026-02-09 23:16:57 +0700

    Fix flake8 and adjust lint rules

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -1,9 +1,17 @@
 from pathlib import Path
+from django.contrib.auth.password_validation import (
+    CommonPasswordValidator,
+    MinimumLengthValidator,
+    NumericPasswordValidator,
+    UserAttributeSimilarityValidator,
+)
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^'
+SECRET_KEY = (
+    'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^'
+)
 
 DEBUG = True
 
@@ -71,18 +79,10 @@ DATABASES = {
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
+    {'NAME': UserAttributeSimilarityValidator},
+    {'NAME': MinimumLengthValidator},
+    {'NAME': CommonPasswordValidator},
+    {'NAME': NumericPasswordValidator},
 ]
 
 
